b13268348aa6456ab8627d576b3cb70e6172cd10
Serhii <serhii.domik@gmail.com>
2025-02-01 17:37:27 +0200

    Created book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    author = models.CharField(max_length=255)
+    inventory = models.IntegerField()  # більше 0
+    daily_fee = models.DecimalField(decimal_places=2, max_digits=4)
+
+    def __str__(self):
+        return self.title
