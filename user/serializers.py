7a7731c262a6686564f68d05635a2080e50ac6ac
Serhii <serhii.domik@gmail.com>
2025-02-26 16:02:11 +0200

    Implemented user app

---
+++ user/serializers.py
@@ -0,0 +1,57 @@
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework import serializers
+from django.utils.translation import gettext as _
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "email", "password", "first_name", "last_name", "is_staff")
+        read_only_fields = ("is_staff",)
+        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.CharField(label=_("Email"))
+    first_name = serializers.CharField(label=_("First name"))
+    last_name = serializers.CharField(label=_("Last name"))
+    password = serializers.CharField(
+        label=_("Password"), style={"input_type": "password"}
+    )
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        first_name = attrs.get("first_name")
+        last_name = attrs.get("last_name")
+        password = attrs.get("password")
+
+        if email and first_name and last_name and password:
+            user = authenticate(email=email, password=password)
+
+            if user:
+                if not user.is_active:
+                    msg = _("User account is disabled.")
+                    raise serializers.ValidationError(
+                        msg, code="authorization"
+                    )
+            else:
+                msg = _("Unable to log in with provided credentials.")
+                raise serializers.ValidationError(msg, code="authorization")
+        else:
+            msg = _("Must include 'username' and 'password'.")
+            raise serializers.ValidationError(msg, code="authorization")
+
+        attrs["user"] = user
+        return attrs
