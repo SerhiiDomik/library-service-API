e2c434838ac30c6e3df81cf88c5177d8dbebb4c3
Serhii <serhii.domik@gmail.com>
2025-03-09 21:26:52 +0200

    Updated user app: permission fix

--- user/views.py
+++ user/views.py
@@ -1,6 +1,5 @@
 from rest_framework import generics
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -10,6 +9,9 @@ from user.serializers import UserSerializer, AuthTokenSerializer
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = [
+        AllowAny,
+    ]
 
 
 class CreateTokenView(ObtainAuthToken):
