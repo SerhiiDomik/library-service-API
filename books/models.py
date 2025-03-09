65d867b9c1bf6cc5860065f3fd62c2e3550348d7
Serhii <serhii.domik@gmail.com>
2025-03-09 20:45:23 +0200

    Updated book app: permissions|paggination|filter

--- books/models.py
+++ books/models.py
@@ -2,8 +2,17 @@ from django.db import models
 
 
 class Book(models.Model):
+    class CoverChoices(models.TextChoices):
+        HARD = "HARD", "Hardcover"
+        SOFT = "SOFT", "Softcover"
+
     title = models.CharField(max_length=255, unique=True)
     author = models.CharField(max_length=255)
+    cover = models.CharField(
+        max_length=4,
+        choices=CoverChoices.choices,
+        default=CoverChoices.SOFT,
+    )
     inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(decimal_places=2, max_digits=10)
 
--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
 from books.models import Book
 from books.permissions import IsAdminOrIfAuthenticatedReadOnly
@@ -8,4 +9,19 @@ from books.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAdminOrIfAuthenticatedReadOnly,]
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
+    pagination_class = PageNumberPagination
+    pagination_class.page_size = 15
+
+    def get_queryset(self):
+        queryset = self.queryset
+        title = self.request.query_params.get("title")
+        author = self.request.query_params.get("author")
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        if author:
+            queryset = queryset.filter(author__icontains=author)
+
+        return queryset.distinct()
